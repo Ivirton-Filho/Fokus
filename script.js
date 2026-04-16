9192921239f5ecfacf2ecf603161bdacdba070e8
ivirton <ivirtonfilho@gmail.com>
2026-04-15 23:55:52 -0300

    refactor(ui): refatorar estrutura DOM dos botões

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-const html = document.querySelector('html');
+const html = document.querySelector("html");
 const focobt = document.querySelector(".app__card-button--foco");
 const curtobt = document.querySelector(".app__card-button--curto");
 const longobt = document.querySelector(".app__card-button--longo");
-const banner = document.querySelector('.app__image');
+const banner = document.querySelector(".app__image");
 
-focobt.addEventListener('click', () => {
-        html.setAttribute('data-contexto', 'foco');
-        banner.setAttribute('src', './imagens/foco.png');
+focobt.addEventListener("click", () => {
+  alterarContexto("foco");
 });
 
-curtobt.addEventListener('click', () => {
-        html.setAttribute("data-contexto", "descanso-curto");
-        banner.setAttribute('src', './imagens/descanso-curto.png');
+curtobt.addEventListener("click", () => {
+  alterarContexto("descanso-curto");
 });
 
-longobt.addEventListener('click', () => {
-        html.setAttribute('data-contexto', 'descanso-longo');
-        banner.setAttribute('src', './imagens/descanso-longo.png');
+longobt.addEventListener("click", () => {
+  alterarContexto("descanso-longo");
+});
 
-}); 
+function alterarContexto(contexto) {
+  html.setAttribute("data-contexto", contexto);
+  banner.setAttribute("src", `./imagens/${contexto}.png`);
+}
